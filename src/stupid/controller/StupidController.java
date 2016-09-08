5ae83716f92a9e0f279848fcfcc31476238e3399
AarooneyFarr <Aarooney.Farr@gmail.com>
2016-09-08 08:39:44 -0600

    Added Console Print

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,5 +1,16 @@
 package stupid.controller;
 
 public class StupidController {
-
+	
+	private String myName;
+	
+	public StupidController(){
+		myName = "Aaron Farr";
+		
+	}
+   public void start(){
+	  
+	   System.out.println("Who is the Meme Master?");
+	   System.out.println(myName);
+   }
 }
